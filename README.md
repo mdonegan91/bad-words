8323696fb4d3a04af7a86d63fa4c5576a451f3ba
Molly Donegan <mollyrdonegan@gmail.com>
2023-01-18 18:12:40 -0800

    add more bad words to omit

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 Describe: omitBadWords()
 
-    Test: "It should replace the value of the badWord"
+    Test: "It should replace the value of 'bad' word"
     Code:
-        const badWord = "zoinks";
-        const text = "holy zoinks crazy";
+        const text = "holy zoinks man";
+        const badWord = "zoinks"
         omitBadWords(badWord, text);
-    Expected Output: holy **** crazy;
+    Expected Output: holy **** man;
+
+       Test: "It should replace the value of 'bad' words"
+    Code:
+        const text = "holy zoinks muppeteer man";
+        omitBadWords(text);
+    Expected Output: holy **** **** man;
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 // const badArray = ["zoinks", "muppeteer", "biffaroni", "loopdaloop"];
 
-function omitBadWords(badWord, text) {
+function omitBadWords(text) {
     const textArray = text.split(" ");
-    console.log('txtarry: ',textArray)
+    const zoinks = "zoinks"
+    const muppeteer = "muppeteer";
+    const biffaroni = "biffaroni";
+    const loopdaloop = "loopdaloop";
     let replacementWord = "****";
     let updatedText = "";
     textArray.forEach(function (element) {
-        if (element.toLowerCase().includes(badWord.toLowerCase())) {
+        if (element.toLowerCase().includes(zoinks.toLowerCase()) || element.toLowerCase().includes(muppeteer.toLowerCase()) || element.toLowerCase().includes(biffaroni.toLowerCase()) || element.toLowerCase().includes(loopdaloop.toLowerCase())) {
             updatedText = updatedText + " " + replacementWord;
         }
         else {
@@ -15,3 +18,4 @@ function omitBadWords(badWord, text) {
     });
     return updatedText.trim();
 }
+
